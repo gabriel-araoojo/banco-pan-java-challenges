501510738d2e17beeb0bda7823181d6fd56c5216
gabriel-araoojo <gabriel.exe.271@gmail.com>
2023-03-07 20:26:26 -0300

    last bootcamp challenge

--- src/Challenge.java
+++ src/Challenge.java
@@ -256,4 +256,34 @@ public class Challenge {
 
         System.out.println(totalPrice);
      }
+
+    public static void calculateTaxes() {
+        Scanner leitor = new Scanner(System.in);
+    double renda = leitor.nextDouble();
+    double imposto = 0;
+ 
+      //TODO: Agora utilizando estrutura condicional implemente em seu código as condições necessárias para o cálculo da taxa de imposto de renda:
+    if(renda <= 2000) {
+      System.out.println("Isento");
+    }
+    
+    else if((renda > 2000) && (renda <= 3000)) {
+      renda -= 2000;
+      imposto += (renda * 0.08);
+      System.out.println("R$ " + String.format("%.2f",imposto));
+    }
+    
+    else if((renda > 3000) && (renda <= 4500)) {
+      renda -= 3000;
+      imposto += (renda * 0.18) + (1000 * 0.08);
+      System.out.println("R$ " + String.format("%.2f",imposto));
+    }
+    
+    else if(renda > 4500) {
+      renda -= 4500;
+      imposto += (renda * 0.28) + (1500 * 0.18) + (1000 * 0.08);
+      System.out.println("R$ " + String.format("%.2f",imposto));
+    }
+    
+    }
 }
